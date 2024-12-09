770a79ede2e80ae47059d1bf5787a93f83883c6e
unknown <dev23.mxpertz@gmail.com>
2024-12-09 15:41:24 +0530

    first commit

---
+++ src/Components/ProviderLayout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "../Store/store";
+import LayoutWrapper from "./LayoutWrapper";
+
+export default function ProviderLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Provider store={store}>
+        <LayoutWrapper>{children}</LayoutWrapper>
+      </Provider>
+    </div>
+  );
+}
